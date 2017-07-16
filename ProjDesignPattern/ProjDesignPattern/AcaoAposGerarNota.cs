878cee483b375e52b0b458f74281335f14903a01
Willian Melo <williangilson_wg@hotmail.com>
2017-07-16 14:44:16 -0300

    Study about Design Pattern Observer

---
+++ ProjDesignPattern/ProjDesignPattern/AcaoAposGerarNota.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjDesignPattern
+{
+    public interface AcaoAposGerarNota
+    {
+        void Executa(NotaFiscal nf);
+    }
+}
---
+++ ProjDesignPattern/ProjDesignPattern/NotaFiscalDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjDesignPattern
+{
+    public class NotaFiscalDao : AcaoAposGerarNota
+    {
+        public void Executa(NotaFiscal nf)
+        {
+            Console.WriteLine("Salvo no banco");
+        }
+    }
+}
