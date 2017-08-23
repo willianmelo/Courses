071c9c467d709294295658d82f2969ba6fcb95d4
Willian Melo <williangilson_wg@hotmail.com>
2017-08-22 22:01:23 -0300

    Inicio estudos MVC 5

---
+++ MVC 5/Financas/Financas/Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Financas.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+    }
+}
