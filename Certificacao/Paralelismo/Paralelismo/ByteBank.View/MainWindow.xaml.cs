78cde6ed92c67e1cfbc485ec2881a75424e8f4c7
Willian Melo <williangilson_wg@hotmail.com>
2017-10-26 20:01:58 -0200

    Paralelismo

---
+++ Certificacao/Paralelismo/Paralelismo/ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using ByteBank.Core.Repository;
+using ByteBank.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            var contas = r_Repositorio.GetContaClientes();
+
+            var contasQtdThread = contas.Count() / 4;
+
+            var contas_parte1 = contas.Take(contasQtdThread);
+            var contas_parte2 = contas.Skip(contasQtdThread).Take(contasQtdThread);
+            var contas_parte3 = contas.Skip(2* contasQtdThread).Take(contasQtdThread);
+            var contas_parte4 = contas.Skip(3* contasQtdThread).Take(contasQtdThread);
+
+            var resultado = new List<string>();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
+
+            var inicio = DateTime.Now;
+
+            Thread thread_parte1 = new Thread(() =>
+            {
+                foreach(var conta in contas_parte1)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            Thread thread_parte2 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte3 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte3)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte4 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            var fim = DateTime.Now;
+
+            AtualizarView(resultado, fim - inicio);
+        }
+
+        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+    }
+}
