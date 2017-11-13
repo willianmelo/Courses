c413ebc5b6c0edb27d22a09974280708834361a3
Willian Melo <williangilson_wg@hotmail.com>
2017-11-13 21:16:05 -0200

    Finalizacao curso paralelismo 1 Alura

--- Certificacao/Paralelismo/ByteBank.View/MainWindow.xaml.cs
+++ Certificacao/Paralelismo/ByteBank.View/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ namespace ByteBank.View
     {
         private readonly ContaClienteRepository r_Repositorio;
         private readonly ContaClienteService r_Servico;
+        private CancellationTokenSource _cts;
 
         public MainWindow()
         {
@@ -25,31 +26,68 @@ namespace ByteBank.View
 
         private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
-            var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
             BtnProcessar.IsEnabled = false;
+            _cts = new CancellationTokenSource();
+
             var contas = r_Repositorio.GetContaClientes();
 
-            var contasQtdThread = contas.Count() / 4;
+            PgsProgresso.Maximum = contas.Count();
 
-            AtualizarView(new List<string>(), TimeSpan.Zero);
+            LimparView();
 
             var inicio = DateTime.Now;
+            btnCancelar.IsEnabled = true;
+            var progress = new Progress<string>(str => PgsProgresso.Value++);
+            
 
-            var resultado = await ConsolidarContas(contas);
+            try {
+                var resultado = await ConsolidarContas(contas, progress, _cts.Token);
+                var fim = DateTime.Now;
+                AtualizarView(resultado, fim - inicio);
+            }
+            catch (OperationCanceledException)
+            {
+                TxtTempo.Text = "Op cancelada pelo usuario";
+            }
+            finally {
+                BtnProcessar.IsEnabled = true;
+                btnCancelar.IsEnabled = false;
+            }
 
-            var fim = DateTime.Now;
-            AtualizarView(resultado, fim - inicio);
+           
 
             BtnProcessar.IsEnabled = true;
         }
 
-        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            btnCancelar.IsEnabled = false;
+            _cts.Cancel();
+        }
+
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> rptProgresso, CancellationToken ct)
         {
             var tasks = contas.Select(conta =>
-                 Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta))
+                 Task.Factory.StartNew(() =>
+                 {
+                     ct.ThrowIfCancellationRequested();
+
+                     var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta, ct);
+                     rptProgresso.Report(resultadoConsolidacao);
+
+                     ct.ThrowIfCancellationRequested();
+                     return resultadoConsolidacao;
+                 }, ct)
             );
 
-            return  await Task.WhenAll(tasks);
+            return await Task.WhenAll(tasks);
+        }
+
+        private void LimparView()
+        {
+            LstResultados.ItemsSource = null;
+            TxtTempo.Text = null;
+            PgsProgresso.Value = 0;
         }
 
         private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
